70008c74a6ff850bf5c59d13c198ba13d4fbc799
kelechi <paulkelechi11@gmail.com>
2023-09-26 23:37:19 +0000

    Added a new script

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,14 @@
 export default function taskBlock(trueOrFalse) {
+  var task;
+  var task2;
 
-    if (trueOrFalse) {
-	var task = true;
-	var task2 = false;
-    }
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
 
-    var task = false;
-    var task2 = true;
+  task = false;
+  task2 = true;
 
   return [task, task2];
 }
