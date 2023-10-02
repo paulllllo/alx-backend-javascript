e1b77fbbfbbdd68f2843b69a1c92711f7d97a3c8
kelechi <paulkelechi11@gmail.com>
2023-10-02 01:07:37 +0000

    Added a script that shows promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,17 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  let body;
+
+  uploadPhoto()
+    .then((value) => {
+      body = value.body;
+    })
+    .then(() => createUser())
+    .then((value) => {
+      console.log(`${body} ${value.firstName} ${value.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
