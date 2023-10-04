85b1d3ca006a6c94255340674c8aaff214911d32
kelechi <paulkelechi11@gmail.com>
2023-10-04 00:48:15 +0000

    Added a module that shows weakmaps

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,6 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const count = weakMap.get(endpoint);
   if (count === undefined) return weakMap.set(endpoint, 1);
-  if (count >= 5) throw new Error('Endpoint load is high');
-
-  return weakMap.set(endpoint, count + 1);
+  weakMap.set(endpoint, count + 1);
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
 }
