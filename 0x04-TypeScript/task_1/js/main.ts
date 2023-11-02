390c1991db4b9da409b91739e2e418a226ec1bbf
kelechi <paulkelechi11@gmail.com>
2023-11-02 13:32:05 +0000

    Added a script that extends a pre-existing interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,6 +2,21 @@ interface Teacher {
 	  firstName: string;
 	  lastName: string;
 	  fullTimeEmployee: boolean;
-	  yearsOfExperience: number;
+	  yearsOfExperience?: number;
 	  location: string;
 }
+
+
+interface Directors extends Teacher {
+	  numberOfReports: number;
+}
+
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
