5650c4e32975e221e42e6177eb2af08573d61c8b
kelechi <paulkelechi11@gmail.com>
2023-11-06 08:48:26 +0000

    Added a script that shows interface with classes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,4 +19,28 @@ const printTeacher: printTeacherFunction = function (firstName: string, lastName
 	 return (`${firstName[0]}. ${lastName}`);
 }
 
-console.log(printTeacher("John", "Doe"));
+interface studentClassShape {
+	  firstName: string;
+	  lastName: string;
+	  workOnHomework: () => "Currently working";
+	  displayName: () => string;
+}
+
+class StudentClass implements studentClassShape {
+      firstName: string;
+      lastName: string;
+
+      constructor(firstName: string, lastName: string) {
+      			     this.firstName = firstName;
+			     this.lastName = lastName;
+      }
+      workOnHomework = (): "Currently working" => {
+      		     return ("Currently working")
+      }
+      displayName = (): string => {
+      		  return this.firstName
+      }
+}
+
+const st1 = new StudentClass("Wale", "Olowu");
+console.log(st1.displayName());
