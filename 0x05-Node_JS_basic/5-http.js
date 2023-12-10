b16f9d0929f3a1ee7604dc3ca4849b7d157cf97c
kelechi <paulkelechi11@gmail.com>
2023-12-10 17:54:54 +0000

    Correction

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,41 +1,65 @@
 const http = require('http');
 const fs = require('fs').promises;
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+
+const countStudents = (path) => fs.readFile(path, 'utf8')
+  .then((data) => {
+    const dataLines = data.split('\n');
+    const cs = [];
+    const swe = [];
+    const output = [];
+    for (let i = 1; i < dataLines.length; i += 1) {
+      const row = dataLines[i].split(',');
+      if (row.length === 4) {
+        if (row[3] === 'CS') {
+          cs.push(row[0]);
+        } else if (row[3] === 'SWE') {
+          swe.push(row[0]);
+        }
+      }
+    }
+    output.push(`Number of students: ${cs.length + swe.length}`);
+    output.push(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+    output.push(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+    return output;
+  })
+  .catch((err) => {
+    if (err.code === 'ENOENT') {
+      throw new Error('Cannot load the database');
+    }
+    throw err;
+  });
 
 const hostname = '127.0.0.1';
 const port = 1245;
-let database = "This is the list of our students\n";
+let database = 'This is the list of our students\n';
 const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
-    switch (req.url) {
-    case "/":
-	res.writeHead(200);
-	res.end("Hello Holberton School!");
-	break
-    case "/students":
-	res.writeHead(200);
-	res.end(database);
-    }
+  switch (req.url) {
+    case '/':
+      res.writeHead(200);
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      res.writeHead(200);
+      res.end(database);
+      break;
+    default:
+      throw new Error();
+  }
 });
 
-exec(`node -e "const countStudents = require('./3-read_file_async'); 
-countStudents('${process.argv[2]}');"`)
-    .then(({stdout, stderr}) => {
-	if (stdout) {
-	    const trim = stdout.slice(0, -1);
-	    database += trim;
-	} else {
-	    database += stderr;
-	}
-	app.listen(port, hostname, () => {
+// exec(`node -e "const countStudents = require('./3-read_file_async');
+// countStudents('${process.argv[2]}');"`)
 
-	});
-	
-    })
-    .catch((err) => {
-	throw new Error(err);
-    })
+countStudents(`${__dirname}/${process.argv[2]}`)
+  .then((output) => {
+    database += output.join('\n');
+    app.listen(port, hostname, () => {
 
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 module.exports = app;
